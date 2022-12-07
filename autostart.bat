79c6bc1d55fb52de8cd270ca85a9c04d97353f74
black2856 <naoki2856@gmail.com>
2022-12-07 14:41:05 +0900

    '2022/12/07_14:41:05.10'

--- autostart.bat
+++ autostart.bat
@@ -1,4 +1,14 @@
+@echo off
+setlocal ENABLEDELAYEDEXPANSION
+set PATHLF=!cd:\=^
+
+!
+
+for /F "delims=\" %%a in ("!PATHLF!") do (set cDIR=%%a)
+endlocal && set cDIR=%cDIR%
+
+
 git pull
 cd ../
-make img=etrobo2023
+make img=%cDIR%
 make start
