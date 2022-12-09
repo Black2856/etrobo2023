71c5a14615a221c2584d21b52674f5ffbc331826
black2856 <naoki2856@gmail.com>
2022-12-09 17:36:01 +0900

    2022/12/09_17:36:01.76

--- makefile.py
+++ makefile.py
@@ -8,19 +8,22 @@ import glob
 import re
 
 # 抽出する拡張子
-pattern = re.compile(r'\.cpp|\.h')
+pattern = re.compile(r'\.c|\.cpp|\.h')
 
 # ディレクトリ抽出
 dirs = [os.path.dirname(x.replace('\\', '/')) for x in glob.glob('**/*/', recursive=True)]
 print(dirs)
 
 # ファイル抽出
-files = [os.path.basename(x.replace('\\', '/')) for x in glob.glob('**/*', recursive=True) if pattern.search(x)]
+files = set([os.path.basename(x.replace('\\', '/')) for x in glob.glob('**/*', recursive=True) if pattern.search(x)])
+files.remove("app.c")
 print(files)
 
-mk_files = set(["   " + x.split(".")[0] + ".o \\" for x in files])
+mk_files = set([x.split(".")[0] for x in files])
+mk_files = set(["   " + x + ".o \\" for x in mk_files if x != "app"])
 mk_dirs1 = set(["   $(mkfile_path)" + x.split(".")[0] + " \\" for x in dirs])
 mk_dirs2 = set(["   -I$(mkfile_path)" + x.split(".")[0] + " \\" for x in dirs])
+
 dic = {
     'mk_files' : mk_files,
     'mk_dirs1' : mk_dirs1,
