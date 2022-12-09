11e65a4cb454ea5792dd8a519eabbf4843c769fd
black2856 <naoki2856@gmail.com>
2022-12-09 16:45:21 +0900

    2022/12/09_16:45:21.36

--- app.cpp
+++ app.cpp
@@ -1,25 +1,55 @@
 #include "app.h"
-#include <stdio.h>
+#include "util.h"
 
-/* メインタスク(起動時にのみ関数コールされる) */
+#include "Motor.h"
+#include "Clock.h"
+
+using namespace ev3api;
+
+/**
+ * メインタスク
+ */
+// tag::main_task_1[]
 void main_task(intptr_t unused) {
 
-    /* センサー入力ポートの設定 */
-    ev3_sensor_config(touch_sensor ,TOUCH_SENSOR);
-    ev3_sensor_config(color_sensor ,COLOR_SENSOR);
-    ev3_sensor_config(sonar_sensor ,ULTRASONIC_SENSOR);
-    ev3_sensor_config(gyro_sensor  ,GYRO_SENSOR);
-    
-    /* モーター出力ポートの設定 */
-    ev3_motor_config(arm_motor     ,LARGE_MOTOR);
-    ev3_motor_config(left_motor    ,MEDIUM_MOTOR);
-    ev3_motor_config(right_motor   ,MEDIUM_MOTOR);
-    
-    printf("Start Line Trace!!\n");
-    
-    /* ライントレースタスクの起動 */
-    sta_cyc(LINE_TRACER_TASK_CYC);
+  Motor leftWheel(PORT_C);
+  Motor rightWheel(PORT_B);
+  Clock clock;
 
-    /* タスク終了 */
-    ext_tsk();
+#ifndef MAKE_RASPIKE
+  const int8_t pwm = (Motor::PWM_MAX) / 6;
+#else
+  const int8_t pwm = 60;
+#endif
+
+  const uint32_t duration = 2000*1000;
+
+  init_f(__FILE__);
+  while(1) {
+    msg_f("Forwarding...", 1);
+    leftWheel.setPWM(pwm);
+    rightWheel.setPWM(pwm);
+    clock.sleep(duration);
+// end::main_task_1[]
+// tag::main_task_2[]
+    msg_f("Backwarding...", 1);
+    leftWheel.setPWM(-pwm);
+    rightWheel.setPWM(-pwm);
+    clock.sleep(duration);
+
+    // 左ボタンを長押し、それを捕捉する
+    if (ev3_button_is_pressed(LEFT_BUTTON)) {
+      break;
+    }
+  }
+
+  msg_f("Stopped.", 1);
+  leftWheel.stop();
+  rightWheel.stop();
+  while(ev3_button_is_pressed(LEFT_BUTTON)) {
+    ;
+  }
+
+  ext_tsk();
 }
+// end::main_task_2[]
--- test/test.cpp
+++
@@ -1,5 +0,0 @@
-#include "test.h"
-
-void test_task(intptr_t unused) {
-
-}
