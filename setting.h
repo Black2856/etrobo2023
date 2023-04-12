0277f38998b01e13924f223d27a6c5381d9eaea0
kiligiris <cays1200431@gn.iwasaki.ac.jp>
2023-04-12 15:20:21 +0900

    add setting.h

---
+++ setting.h
@@ -0,0 +1,12 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+// デバイスのポート設定
+#define SONAR_SENSOR PORT_4
+#define TOUCH_SENSOR PORT_3
+#define GYRO_SENSOR PORT_2
+#define COLOR_SENSOR PORT_1
+#define LEFT_WHEEL PORT_C
+#define RIGHT_WHEEL PORT_B
+
+#endif  // SETTINGS_H
