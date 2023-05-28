86f4ed93aaafbe6ceb813e558be856411667c15f
kiligiris <cays1200431@gn.iwasaki.ac.jp>
2023-05-28 09:21:09 +0900

    add JudgeMileage

---
+++ unit/judge/JudgeMileage.cpp
@@ -0,0 +1,15 @@
+/*
+ * クラス名:JudgeTime
+ * 作成日：2023/05/28
+ * 作成者:杉本
+ */
+#include "JudgeMileage.h"
+
+JudgeMileage::JudgeMileage():
+    JudgeNumerical(),
+    localization(Localization::getInstance()){
+}
+
+float JudgeMileage::getValue() const {
+    return localization.getDistance();
+}
