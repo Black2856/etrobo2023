fd8d4e2150fbbb31f267cf5b70b35fc3c3b9f6d2
kiligiris <kiligiris101064@gmail.com>
2023-07-12 12:05:34 +0900

    fix

--- util/compile/RearCamera.sh
+++ util/compile/RearCamera.sh
@@ -2,6 +2,6 @@
 
 devicepath="./spike/device/"
 
-g++ "$devicepath"RearCamera.c -o "$devicepath"tekephoto -I/usr/include/opencv4/ -lopencv_core -lopencv_videoio -lopencv_imgcodecs
+g++ "$devicepath"RearCamera.c -o "$devicepath"takephoto -I/usr/include/opencv4/ -lopencv_core -lopencv_videoio -lopencv_imgcodecs
 
 exit 0
