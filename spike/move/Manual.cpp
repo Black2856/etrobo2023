7d70cad2a20bf52cce9045c3da96abab9bf94997
Black2856 <naoki2856@gmail.com>
2023-08-03 16:28:34 +0900

    2023/08/03_16:28:33.98

--- spike/move/Manual.cpp
+++ spike/move/Manual.cpp
@@ -44,9 +44,8 @@ void Manual::straight(){
     //float differenceCount = calc.localization.getDifferenceCount();
     float differenceDirection = this->standardDirection - this->calc.localization.getDirection();
     int gain = int(std::sqrt(differenceDirection) * 2.5 + 0.5);
-    printf("%d, ",gain);
-    this->device.LWheel_setPWM(correctionPWM);
-    this->device.RWheel_setPWM(correctionPWM);
+    this->device.LWheel_setPWM(correctionPWM - gain);
+    this->device.RWheel_setPWM(correctionPWM + gain);
 }
 
 void Manual::centerRotation(){
