758ce8e951c6b8d5b36c11b9cfd51bfa73eff1ba
Black2856 <naoki2856@gmail.com>
2023-08-03 21:51:54 +0900

    2023/08/03_21:51:54.49

--- spike/move/Manual.cpp
+++ spike/move/Manual.cpp
@@ -20,7 +20,8 @@ void Manual::first(RunType runType, float pwm, float pwmTransitionTime){
     this->runType = runType;
     this->setPWM(pwm, pwmTransitionTime);
     this->calc.localization.resetDifferenceCount();
-    this->standardDirection = this->calc.localization.getDirection();
+    //this->standardDirection = this->calc.localization.getDirection();
+    this->standardDirection = this->device.gyro_getAngle();
 }
 
 void Manual::execute(){
@@ -43,7 +44,8 @@ void Manual::execute(){
 void Manual::straight(){
     int correctionPWM = this->calc.pwmCalc.changePWM();
     //直進移動になるように補正する
-    float differenceDirection = this->standardDirection - this->calc.localization.getDirection();
+    //float differenceDirection = this->standardDirection - this->calc.localization.getDirection();
+    float differenceDirection = this->standardDirection - this->device.gyro_getAngle();
     int gain = int(this->straightPID.calc(differenceDirection, 0));
     //int gain = int(differenceDirection * 2.3 + 0.5);
     this->device.LWheel_setPWM(correctionPWM + gain);
