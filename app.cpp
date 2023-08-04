ce1d958de54b63d3d4fef9a105932e03c76c86fb
black2856 <naoki2856@gmail.com>
2023-08-04 18:02:17 +0900

    2023/08/04_18:02:17.64

--- app.cpp
+++ app.cpp
@@ -36,33 +36,32 @@ main.makeCALL_SCENARIO("run");
 manage_scenario.add(main);
 
 Manage_scene calibration("calibration");
-calibration.makeSTOP(0, 0);
-calibration.makeMANUAL(1, 2, 50.0, 0.3);
-calibration.makeSTOP(2, 0);
+calibration.makeMANUAL(0, 2, 50.0, 0.3);
+calibration.makeSTOP(1, 0);
 calibration.makeCALIBRATION("record");
-calibration.makeMANUAL(3, 2, -50.0, 0.15);
-calibration.makeSTOP(4, 0);
+calibration.makeMANUAL(2, 2, -50.0, 0.15);
+calibration.makeSTOP(3, 0);
 calibration.makeCALIBRATION("stop");
-calibration.makeON_LINE_MOVE(5, 50, -0.4, 0.0, 0.3);
-calibration.makeSTOP(6, 0);
+calibration.makeON_LINE_MOVE(4, 50, -0.4, 0.0, 0.3);
+calibration.makeSTOP(5, 0);
 manage_scenario.add(calibration);
 
 Manage_scene ready("ready");
-ready.makeSTOP(7, 0.0);
+ready.makeSTOP(6, 0.0);
 manage_scenario.add(ready);
 
 Manage_scene run("run");
-run.makeTRACE(8, 80.0, 0.7, 0.0, 0.4, 1.0);
-run.makeTRACE(9, 50.0, 0.4, 0.0, 0.2, 1.0);
-run.makeSTOP(10, 1.0);
-run.makeMANUAL(11, 2.0, 45.0, 0.0);
-run.makeSTOP(12, 0.0);
+run.makeTRACE(7, 70.0, 1.5, 0.0, 0.8, 0.05);
+run.makeTRACE(8, 50.0, 0.4, 0.0, 0.2, 0.1);
+run.makeSTOP(9, 1.0);
+run.makeMANUAL(10, 2.0, 45.0, 0.0);
+run.makeSTOP(11, 0.0);
 manage_scenario.add(run);
 
 Manage_scene test("test");
-test.makeSTOP(13, 0.0);
-test.makeMANUAL(14, 1.0, 100.0, 0.1);
-test.makeSTOP(15, 0.0);
+test.makeSTOP(12, 0.0);
+test.makeMANUAL(13, 1.0, 100.0, 0.1);
+test.makeSTOP(14, 0.0);
 manage_scenario.add(test);
 
   //#</make_scenario>
--- spike/scenario/state_transition.cpp
+++ spike/scenario/state_transition.cpp
@@ -17,22 +17,21 @@ bool StateTransition::judge(int idx){
 
     switch(idx){
     //#<func_list>
-        case 0: func = [](Judge& j) {return j.time.j(3000 * 1000);}; break;
-        case 1: func = [](Judge& j) {return j.angle.j(360);}; break;
-        case 2: func = [](Judge& j) {return j.time.j(3000 * 1000);}; break;
-        case 3: func = [](Judge& j) {return j.angle.j(-460);}; break;
-        case 4: func = [](Judge& j) {return j.time.j(1000 * 1000);}; break;
-        case 5: func = [](Judge& j) {return j.ret.j(true) and j.time.j(5000 * 1000);}; break;
-        case 6: func = [](Judge& j) {return j.time.j(1000 * 1000);}; break;
-        case 7: func = [](Judge& j) {return j.touch.j(true);}; break;
-        case 8: func = [](Judge& j) {return j.time.j(2000 * 1000);}; break;
-        case 9: func = [](Judge& j) {return j.time.j(3000 * 1000);}; break;
-        case 10: func = [](Judge& j) {return j.time.j(2000 * 1000);}; break;
-        case 11: func = [](Judge& j) {return j.angle.j(90);}; break;
-        case 12: func = [](Judge& j) {return j.time.j(1000 * 1000);}; break;
-        case 13: func = [](Judge& j) {return j.time.j(5000 * 1000);}; break;
-        case 14: func = [](Judge& j) {return j.time.j(20000 * 1000);}; break;
-        case 15: func = [](Judge& j) {return j.time.j(30000 * 1000);}; break;
+        case 0: func = [](Judge& j) {return j.angle.j(10);}; break;
+        case 1: func = [](Judge& j) {return j.time.j(3000 * 1000);}; break;
+        case 2: func = [](Judge& j) {return j.angle.j(-460);}; break;
+        case 3: func = [](Judge& j) {return j.time.j(1000 * 1000);}; break;
+        case 4: func = [](Judge& j) {return j.ret.j(true) and j.time.j(5000 * 1000);}; break;
+        case 5: func = [](Judge& j) {return j.time.j(1000 * 1000);}; break;
+        case 6: func = [](Judge& j) {return j.touch.j(true);}; break;
+        case 7: func = [](Judge& j) {return j.time.j(30000 * 1000);}; break;
+        case 8: func = [](Judge& j) {return j.time.j(3000 * 1000);}; break;
+        case 9: func = [](Judge& j) {return j.time.j(2000 * 1000);}; break;
+        case 10: func = [](Judge& j) {return j.angle.j(90);}; break;
+        case 11: func = [](Judge& j) {return j.time.j(1000 * 1000);}; break;
+        case 12: func = [](Judge& j) {return j.time.j(5000 * 1000);}; break;
+        case 13: func = [](Judge& j) {return j.time.j(20000 * 1000);}; break;
+        case 14: func = [](Judge& j) {return j.time.j(30000 * 1000);}; break;
     //#</func_list>
         default: return true;
     }
