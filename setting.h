c34d83eb0a1b03d14b06b4ac06cc6cf9ddb378a2
Black2856 <naoki2856@gmail.com>
2023-08-05 07:22:41 +0900

    2023/08/05_ 7:22:41.11

--- setting.h
+++ setting.h
@@ -20,8 +20,8 @@
 #define RIGHT_WHEEL_POWER 1.0 //右車輪の出力量
 #else  //SPIKE
 #define TREAD 126.0
-#define LEFT_WHEEL_POWER 0.96
-#define RIGHT_WHEEL_POWER 1.0
+#define LEFT_WHEEL_POWER 1.0
+#define RIGHT_WHEEL_POWER 0.95
 #endif // MAKE_SIM
 
 #define CAMERA_NUMBER 0 // カメラデバイス番号（通常は0）を指定
