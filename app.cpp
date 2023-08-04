6a38bd8af7b5a453b3448b235720b4110c6d7b8b
black2856 <naoki2856@gmail.com>
2023-08-04 17:56:14 +0900

    2023/08/04_17:56:14.67

--- app.cpp
+++ app.cpp
@@ -36,31 +36,33 @@ main.makeCALL_SCENARIO("run");
 manage_scenario.add(main);
 
 Manage_scene calibration("calibration");
-calibration.makeMANUAL(0, 2, 50.0, 0.3);
-calibration.makeCALIBRATION("record");
-calibration.makeMANUAL(1, 2, -50.0, 0.15);
+calibration.makeSTOP(0, 0);
+calibration.makeMANUAL(1, 2, 50.0, 0.3);
 calibration.makeSTOP(2, 0);
-calibration.makeCALIBRATION("stop");
-calibration.makeON_LINE_MOVE(3, 50, -0.4, 0.0, 0.3);
+calibration.makeCALIBRATION("record");
+calibration.makeMANUAL(3, 2, -50.0, 0.15);
 calibration.makeSTOP(4, 0);
+calibration.makeCALIBRATION("stop");
+calibration.makeON_LINE_MOVE(5, 50, -0.4, 0.0, 0.3);
+calibration.makeSTOP(6, 0);
 manage_scenario.add(calibration);
 
 Manage_scene ready("ready");
-ready.makeSTOP(5, 0.0);
+ready.makeSTOP(7, 0.0);
 manage_scenario.add(ready);
 
 Manage_scene run("run");
-run.makeTRACE(6, 80.0, 0.7, 0.0, 0.4, 1.0);
-run.makeTRACE(7, 50.0, 0.4, 0.0, 0.2, 1.0);
-run.makeSTOP(8, 1.0);
-run.makeMANUAL(9, 2.0, 45.0, 0.0);
-run.makeSTOP(10, 0.0);
+run.makeTRACE(8, 80.0, 0.7, 0.0, 0.4, 1.0);
+run.makeTRACE(9, 50.0, 0.4, 0.0, 0.2, 1.0);
+run.makeSTOP(10, 1.0);
+run.makeMANUAL(11, 2.0, 45.0, 0.0);
+run.makeSTOP(12, 0.0);
 manage_scenario.add(run);
 
 Manage_scene test("test");
-test.makeSTOP(11, 0.0);
-test.makeMANUAL(12, 1.0, 100.0, 0.1);
 test.makeSTOP(13, 0.0);
+test.makeMANUAL(14, 1.0, 100.0, 0.1);
+test.makeSTOP(15, 0.0);
 manage_scenario.add(test);
 
   //#</make_scenario>
