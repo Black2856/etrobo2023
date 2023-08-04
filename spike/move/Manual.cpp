337a706c6eb1301861f8cd38e2e18bf810ea111a
Black2856 <naoki2856@gmail.com>
2023-08-05 08:08:03 +0900

    2023/08/05_ 8:08:03.48

--- spike/move/Manual.cpp
+++ spike/move/Manual.cpp
@@ -8,7 +8,7 @@
 
 Manual::Manual():
     device(DeviceInOut::getInstance()){
-        unit::pid_t pid1 = {2.3, 0.022, 0.05};
+        unit::pid_t pid1 = {2.3, 0.0, 0.05};
         this->straightPID.setPID(pid1);
         unit::pid_t pid2 = {2.3, 0.022, 0.15};
         this->centerPID.setPID(pid2);
