09cd8c8ca8c558d32639b3aeed2270a740326917
black2856 <naoki2856@gmail.com>
2023-08-05 15:58:47 +0900

    2023/08/05_15:58:47.49

--- app.cpp
+++ app.cpp
@@ -29,10 +29,7 @@ void Walker::run() {
 
   //#<make_scenario>
 Manage_scene main("main");
-main.makeCALL_SCENARIO("ready");
-main.makeCALL_SCENARIO("calibration");
-main.makeCALL_SCENARIO("ready");
-main.makeCALL_SCENARIO("run");
+main.makeCALL_SCENARIO("test");
 manage_scenario.add(main);
 
 Manage_scene calibration("calibration");
@@ -59,12 +56,8 @@ manage_scenario.add(run);
 
 Manage_scene test("test");
 test.makeSTOP(11, 0.0);
-test.makeMANUAL_PID(12, 1.0, 50.0, 3000.0);
+test.makeMANUAL_PID(12, 1.0, 95.0, 4000.0);
 test.makeSTOP(13, 0.0);
-test.makeMANUAL_PID(14, 2.0, 50.0, 180.0);
-test.makeSTOP(15, 0.0);
-test.makeMANUAL_PID(16, 2.0, 50.0, -180.0);
-test.makeSTOP(17, 0.0);
 manage_scenario.add(test);
 
   //#</make_scenario>
--- spike/scenario/state_transition.cpp
+++ spike/scenario/state_transition.cpp
@@ -29,12 +29,8 @@ bool StateTransition::judge(int idx){
         case 9: func = [](Judge& j) {return j.mileage.j(1000);}; break;
         case 10: func = [](Judge& j) {return j.time.j(3000 * 1000);}; break;
         case 11: func = [](Judge& j) {return j.time.j(5000 * 1000);}; break;
-        case 12: func = [](Judge& j) {return j.ret.j(true) and j.time.j(20000 * 1000);}; break;
+        case 12: func = [](Judge& j) {return j.ret.j(true) and j.time.j(10000 * 1000);}; break;
         case 13: func = [](Judge& j) {return j.time.j(7000 * 1000);}; break;
-        case 14: func = [](Judge& j) {return j.ret.j(true) and j.time.j(20000 * 1000);}; break;
-        case 15: func = [](Judge& j) {return j.time.j(3000 * 1000);}; break;
-        case 16: func = [](Judge& j) {return j.ret.j(true) and j.time.j(20000 * 1000);}; break;
-        case 17: func = [](Judge& j) {return j.time.j(60000 * 1000);}; break;
     //#</func_list>
         default: return true;
     }
