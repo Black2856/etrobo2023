7bc8f8dc1347b054de9649bf3bd831b6a7c5f2ee
kiligiris <cays1200431@gn.iwasaki.ac.jp>
2023-09-21 21:54:51 +0900

    fix Signal.py

--- PC/manage_signal/Signal.py
+++ PC/manage_signal/Signal.py
@@ -3,12 +3,19 @@ import numpy as np
 import socket
 import struct
 
-IP_ADDR = "127.0.0.10"
+IP_ADDR = "0.0.0.0"
 PORT = 8080
+# 環境差調整用定数
+SIZE_LEN = 8
+if(SIZE_LEN == 4):
+    FORMAT = "I"
+elif(SIZE_LEN == 8):
+    FORMAT = "Q"
 
 class Signal:
     def __init__(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+        self.conn = None
         self.socket.settimeout(120)
         self.socket.setblocking(True)
 
@@ -17,73 +24,86 @@ class Signal:
 
         
     def open(self) -> bool:
-        self.socket.bind((IP_ADDR, PORT))
-        self.socket.listen(1)
-        print("Server started : ", IP_ADDR, ":", PORT, sep='')
-        return True
+        try:
+            self.socket.bind((IP_ADDR, PORT))
+            self.socket.listen(1)
+            print("Server started : ", IP_ADDR, ":", PORT, sep='')
+            self.conn, addr = self.socket.accept()
+            print('Connected by', addr)
+            return True
+        except:
+            return False
 
     def close(self) -> None:
         try:
+            self.conn.close()
             self.socket.shutdown(socket.SHUT_RDWR)
             self.socket.close()
         except:
             pass
         
     # 広範的データを受信する関数
-    def recvItem(self, conn):
-        with conn:
-            # データのサイズを受信
-            buffer_size = conn.recv(8)
-            # バイナリデータを数値に変換
-            buffer_size = struct.unpack('Q', buffer_size)[0]
-            print("len :", buffer_size)
+    def recvItem(self):
+        # データのサイズを受信
+        buffer_size = self.conn.recv(SIZE_LEN)
+        print("  data size :", buffer_size)
+        # バイナリデータを数値に変換
+        buffer_size = struct.unpack(FORMAT, buffer_size)[0]
+        print("  len       :", buffer_size)
 
-            # データを受信
-            data = b''
-            while len(data) < buffer_size:
-                packet = conn.recv(buffer_size - len(data))
-                if not packet:
-                    break
-                data += packet
-
+        # データを受信
+        data = b''
+        while len(data) < buffer_size:
+            packet = self.conn.recv(buffer_size - len(data))
+            if not packet:
+                break
+            data += packet
+        print("  data      :", data[:20])
+        print("  Succeed")
         return data
     
     # 画像データを受信する関数
-    def recvImage(self, conn):
+    def recvImage(self) -> np.ndarray:
+        print("Recv Image")
         # バイナリデータを受信
-        binary_data = self.recvItem(conn)
-
+        binary_data = self.recvItem()
         # 受信したデータをデコード
         frame_data = np.frombuffer(binary_data, dtype=np.uint8)
 
         # データを画像に変換
         frame = cv2.imdecode(frame_data, 1)
-
         return frame
     
     # 文字列データを受信する関数
-    def recvString(self, conn):
-        data = self.recvItem(conn)
+    def recvString(self) -> str:
+        print("Recv String")
+        data = self.recvItem()
         # 受信したデータをデコード
         st = data.decode('utf-8')
         return st
-    
-    def accept(self):
-        conn, addr = self.socket.accept()
-        print('Connected by', addr)
-        return conn
+
+    # 文字列を送信する関数
+    def sendString(self, text) -> None:
+        print("Send String")
+        data = text.encode('utf-8')
+        size = struct.pack(FORMAT, len(data))
+        print("  len       :", len(data))
+        print("  data      :", data[:20])
+        
+        self.conn.sendall(size)
+        self.conn.sendall(data)
+        print("  Succeed")
         
 
 if __name__ == "__main__":
     signal = Signal()
     signal.open()
-    conn = signal.accept()
 
-    image  = signal.recvImage(conn)
-    #string = signal.recvString(conn)
-    
-    conn.close()
+    img = signal.recvImage()
+    string = signal.recvString()
+    signal.sendString("hello")
+
     signal.close()
         
     # 画像を保存
-    #cv2.imwrite(f'{string}', image)
+    cv2.imwrite(f'{string}', img)
