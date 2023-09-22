61890727aa84de865961d6824f6021d2e8df6375
black2856 <naoki2856@gmail.com>
2023-09-22 13:18:13 +0900

    2023/09/22_13:18:11.77

--- spike/judge/JudgeColor.cpp
+++ spike/judge/JudgeColor.cpp
@@ -15,24 +15,27 @@ bool JudgeColor::j(unit::color_t color){
 unit::color_t JudgeColor::getColor(){
     this->rgb2hsv.update();
     unit::hsv_t hsv = this->rgb2hsv.getHSV();
+
+    // テスト表示
+    printf("<%d, %d, %d>", hsv.h, hsv.s, hsv.v);
     
-    if((0 <= hsv.h && hsv.h <= 0) && (0 <= hsv.s && hsv.s <= 0) && (0 <= hsv.v && hsv.v <= 0)){
+    if((0 <= hsv.h && hsv.h <= 360) && (5 <= hsv.s && hsv.s <= 60) && (0 <= hsv.v && hsv.v <= 30)){
         return unit::color_t::BLACK;
 
-    }else if((0 <= hsv.h && hsv.h <= 0) && (0 <= hsv.s && hsv.s <= 0) && (0 <= hsv.v && hsv.v <= 0)){
+    }else if((0 <= hsv.h && hsv.h <= 360) && (0 <= hsv.s && hsv.s <= 25) && (50 <= hsv.v && hsv.v <= 100)){
         return unit::color_t::WHITE;
 
-    }else if((0 <= hsv.h && hsv.h <= 0) && (0 <= hsv.s && hsv.s <= 0) && (0 <= hsv.v && hsv.v <= 0)){
+    }else if((195 <= hsv.h && hsv.h <= 235) && (60 <= hsv.s && hsv.s <= 100) && (45 <= hsv.v && hsv.v <= 100)){
+        return unit::color_t::BLUE;
+
+    }else if((35 <= hsv.h && hsv.h <= 75) && (60 <= hsv.s && hsv.s <= 100) && (60 <= hsv.v && hsv.v <= 100)){
         return unit::color_t::YELLOW;
 
-    }else if((0 <= hsv.h && hsv.h <= 0) && (0 <= hsv.s && hsv.s <= 0) && (0 <= hsv.v && hsv.v <= 0)){
+    }else if((0 <= hsv.h && hsv.h <= 30) && (50 <= hsv.s && hsv.s <= 100) && (60 <= hsv.v && hsv.v <= 100)){
         return unit::color_t::RED;
 
-    }else if((0 <= hsv.h && hsv.h <= 0) && (0 <= hsv.s && hsv.s <= 0) && (0 <= hsv.v && hsv.v <= 0)){
+    }else if((120 <= hsv.h && hsv.h <= 160) && (50 <= hsv.s && hsv.s <= 100) && (45 <= hsv.v && hsv.v <= 100)){
         return unit::color_t::GREEN;
-        
-    }else if((0 <= hsv.h && hsv.h <= 0) && (0 <= hsv.s && hsv.s <= 0) && (0 <= hsv.v && hsv.v <= 0)){
-        return unit::color_t::BLUE;
     }
 
     return unit::color_t::WHITE;
