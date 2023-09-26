c9a87e299062a4bee3fe18584d8a501625e03012
kotaro <cakm1200141@gn.iwasaki.ac.jp>
2023-09-26 17:21:34 +0900

    2023/09/26_17:21:34.79

--- app.cpp
+++ app.cpp
@@ -58,7 +58,7 @@ run.makeTRACE(8, 85.0, -0.055, 0.0, -0.07, 0.5);
 run.makeTRACE(9, 65.0, -0.9, 0.0, -0.3, 0.4);
 run.makeTRACE(10, 85.0, -0.055, 0.0, -0.07, 0.5);
 run.makeTRACE(11, 65.0, -0.8, 0.0, -0.3, 0.3);
-run.makeTRACE(12, 85.0, 0.1, 0.0, 0.1, 0.5);
+run.makeTRACE(12, 65.0, 0.1, 0.0, 0.1, 0.5);
 run.makeSTOP(13, 0.0);
 manage_scenario.add(run);
 
