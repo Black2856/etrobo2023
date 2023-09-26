2dd75fa91e2603ecd8b7bbcf0172c86113e50901
kotaro <cakm1200141@gn.iwasaki.ac.jp>
2023-09-26 18:40:57 +0900

    2023/09/26_18:40:57.83

--- app.cpp
+++ app.cpp
@@ -61,23 +61,27 @@ run.makeTRACE(11, 60.0, -0.84, 0.0, -0.3, 0.3);
 run.makeMANUAL_PID(12, 2.0, 60.0, -15.0);
 run.makeTRACE(13, 55.0, 0.4, 0.0, 0.15, 0.5);
 run.makeTRACE(14, 55.0, 0.4, 0.0, 0.15, 0.5);
-run.makeSTOP(15, 0.0);
+run.makeMANUAL_PID(15, 2.0, 60.0, 15.0);
+run.makeTRACE(16, 55.0, -0.4, 0.0, -0.15, 0.5);
+run.makeMANUAL_PID(17, 2.0, 60.0, -15.0);
+run.makeTRACE(18, 55.0, 0.4, 0.0, 0.15, 0.5);
+run.makeSTOP(19, 0.0);
 manage_scenario.add(run);
 
 Manage_scene zero4("zero4");
-zero4.makeSTOP(16, 0.0);
-zero4.makeMANUAL_PID(17, 1.0, 90.0, 4000.0);
-zero4.makeSTOP(18, 0.0);
+zero4.makeSTOP(20, 0.0);
+zero4.makeMANUAL_PID(21, 1.0, 90.0, 4000.0);
+zero4.makeSTOP(22, 0.0);
 manage_scenario.add(zero4);
 
 Manage_scene test("test");
-test.makeSTOP(19, 0.0);
+test.makeSTOP(23, 0.0);
 manage_scenario.add(test);
 
 Manage_scene turn("turn");
-turn.makeMANUAL_PID(20, 2.0, 60.0, -90.0);
-turn.makeMANUAL(21, 1.0, 60.0, 0.0);
-turn.makeSTOP(22, 0.0);
+turn.makeMANUAL_PID(24, 2.0, 60.0, -90.0);
+turn.makeMANUAL(25, 1.0, 60.0, 0.0);
+turn.makeSTOP(26, 0.0);
 manage_scenario.add(turn);
 
     //#</make_scenario>
