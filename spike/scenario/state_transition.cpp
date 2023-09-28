ca7fe0016efc0ad82c4fe30054b65c80822b10d3
kotaro <cakm1200141@gn.iwasaki.ac.jp>
2023-09-28 21:57:52 +0900

    2023/09/28_21:57:52.46

--- spike/scenario/state_transition.cpp
+++ spike/scenario/state_transition.cpp
@@ -38,7 +38,7 @@ bool StateTransition::judge(int idx){
         case 18: func = [](Judge& j) {return j.time.j(100 * 1000);}; break;
         case 19: func = [](Judge& j) {return j.mileage.j(2000);}; break;
         case 20: func = [](Judge& j) {return j.mileage.j(600);}; break;
-        case 21: func = [](Judge& j) {return j.mileage.j(1600);}; break;
+        case 21: func = [](Judge& j) {return j.mileage.j(600);}; break;
         case 22: func = [](Judge& j) {return j.time.j(100 * 1000);}; break;
         case 23: func = [](Judge& j) {return j.ret.j(true);}; break;
         case 24: func = [](Judge& j) {return j.ret.j(true);}; break;
