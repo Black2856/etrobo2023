0a682b9459278bccfaf0e29ee165f19f1aacf252
kotaro <cakm1200141@gn.iwasaki.ac.jp>
2023-09-28 14:17:10 +0900

    2023/09/28_14:17:10.03

--- setting.h
+++ setting.h
@@ -29,7 +29,7 @@
 #define FRICTIONAL 1.10 //zero4は0.8845 0.915 本番:0.9455
 #endif // MAKE_SIM
 
-#define CAMERA_NUMBER 1 // カメラデバイス番号（通常は0）を指定
+#define CAMERA_NUMBER 0 // カメラデバイス番号（通常は0）を指定
 #define CYCLE 1000 * 10 // 処理周期(usec)
 #define IMG_QUEUE "Queue.txt" // 撮影待機リストファイル名
 #ifdef MAKE_SIM
