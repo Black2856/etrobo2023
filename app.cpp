424794a967c1f923e2df465a65e4661df5b3e49e
black2856 <naoki2856@gmail.com>
2023-09-29 13:45:24 +0900

    2023/09/29_13:45:24.61

--- app.cpp
+++ app.cpp
@@ -40,46 +40,47 @@ manage_scenario.add(main);
 
 Manage_scene calibration("calibration");
 calibration.makeSTOP(0, 0);
+calibration.makeTAKE_PHOTO(1, 1);
 calibration.makeCALIBRATION("record:brightness");
-calibration.makeMANUAL_PID(1, 2, 70.0, 450.0);
+calibration.makeMANUAL_PID(2, 2, 70.0, 450.0);
 calibration.makeCALIBRATION("stop");
-calibration.makeSTOP(2, 0);
-calibration.makeMANUAL_PID(3, 5, 80.0, 1.0);
-calibration.makeSTOP(4, 0);
+calibration.makeSTOP(3, 0);
+calibration.makeMANUAL_PID(4, 5, 80.0, 1.0);
+calibration.makeSTOP(5, 0);
 manage_scenario.add(calibration);
 
 Manage_scene ready("ready");
-ready.makeSTOP(5, 0.0);
+ready.makeSTOP(6, 0.0);
 manage_scenario.add(ready);
 
 Manage_scene run("run");
-run.makeTRACE(6, 85.0, -0.12, 0.0, -0.1, 0.5);
-run.makeTRACE(7, 65.0, -1.1, 0.0, -1.0, 0.4);
-run.makeTRACE(8, 85.0, -0.12, 0.0, -0.1, 0.5);
-run.makeTRACE(9, 60.0, -1.1, 0.0, -1.0, 0.3);
-run.makeSTOP(10, 0.0);
+run.makeTRACE(7, 85.0, -0.12, 0.0, -0.1, 0.5);
+run.makeTRACE(8, 65.0, -1.1, 0.0, -1.0, 0.4);
+run.makeTRACE(9, 85.0, -0.12, 0.0, -0.1, 0.5);
+run.makeTRACE(10, 60.0, -1.1, 0.0, -1.0, 0.3);
+run.makeSTOP(11, 0.0);
 manage_scenario.add(run);
 
 Manage_scene doubleloop("doubleloop");
-doubleloop.makeMANUAL_PID(11, 2.0, 60.0, -15.0);
-doubleloop.makeTRACE(12, 75.0, 0.57, 0.0, 0.32, 0.5);
-doubleloop.makeTRACE(13, 75.0, 0.5, 0.0, 0.3, 0.5);
-doubleloop.makeMANUAL_PID(14, 2.0, 60.0, 15.0);
-doubleloop.makeTRACE(15, 50.0, -0.7, 0.0, -0.4, 0.5);
-doubleloop.makeMANUAL_PID(16, 2.0, 60.0, 20.0);
-doubleloop.makeTRACE(17, 60.0, -0.7, 0.0, -0.4, 0.5);
-doubleloop.makeMANUAL_PID(18, 1.0, 60.0, 150.0);
-doubleloop.makeMANUAL_PID(19, 2.0, 55.0, -5.0);
-doubleloop.makeTRACE(20, 55.0, -1.0, 0.0, -1.0, 0.5);
-doubleloop.makeTRACE(21, 60.0, -1.5, 0.0, -1.0, 0.3);
-doubleloop.makeTRACE(22, 85.0, -0.12, 0.0, -0.1, 0.5);
-doubleloop.makeSTOP(23, 0.0);
+doubleloop.makeMANUAL_PID(12, 2.0, 60.0, -15.0);
+doubleloop.makeTRACE(13, 75.0, 0.57, 0.0, 0.32, 0.5);
+doubleloop.makeTRACE(14, 75.0, 0.5, 0.0, 0.3, 0.5);
+doubleloop.makeMANUAL_PID(15, 2.0, 60.0, 15.0);
+doubleloop.makeTRACE(16, 50.0, -0.7, 0.0, -0.4, 0.5);
+doubleloop.makeMANUAL_PID(17, 2.0, 60.0, 20.0);
+doubleloop.makeTRACE(18, 60.0, -0.7, 0.0, -0.4, 0.5);
+doubleloop.makeMANUAL_PID(19, 1.0, 60.0, 150.0);
+doubleloop.makeMANUAL_PID(20, 2.0, 55.0, -5.0);
+doubleloop.makeTRACE(21, 55.0, -1.0, 0.0, -1.0, 0.5);
+doubleloop.makeTRACE(22, 60.0, -1.5, 0.0, -1.0, 0.3);
+doubleloop.makeTRACE(23, 85.0, -0.12, 0.0, -0.1, 0.5);
+doubleloop.makeSTOP(24, 0.0);
 manage_scenario.add(doubleloop);
 
 Manage_scene block("block");
-block.makeMANUAL_PID(24, 2.0, 60.0, -90.0);
-block.makeMANUAL_PID(25, 1.0, 50.0, 50.0);
-block.makeSTOP(26, 0.0);
+block.makeMANUAL_PID(25, 2.0, 60.0, -90.0);
+block.makeMANUAL_PID(26, 1.0, 50.0, 50.0);
+block.makeSTOP(27, 0.0);
 manage_scenario.add(block);
 
     //#</make_scenario>
--- spike/scenario/state_transition.cpp
+++ spike/scenario/state_transition.cpp
@@ -18,32 +18,33 @@ bool StateTransition::judge(int idx){
     switch(idx){
     //#<func_list>
         case 0: func = [](Judge& j) {return j.time.j(500 * 1000);}; break;
-        case 1: func = [](Judge& j) {return j.ret.j(true) and j.time.j(1500 * 1000);}; break;
-        case 2: func = [](Judge& j) {return j.time.j(200 * 1000);}; break;
-        case 3: func = [](Judge& j) {return j.ret.j(true) and j.time.j(1500 * 1000);}; break;
-        case 4: func = [](Judge& j) {return j.time.j(100 * 1000);}; break;
-        case 5: func = [](Judge& j) {return j.button.j(4, true) or j.touch.j(true);}; break;
-        case 6: func = [](Judge& j) {return j.mileage.j(2700);}; break;
-        case 7: func = [](Judge& j) {return j.mileage.j(600);}; break;
-        case 8: func = [](Judge& j) {return j.mileage.j(1600);}; break;
-        case 9: func = [](Judge& j) {return j.mileage.j(550);}; break;
-        case 10: func = [](Judge& j) {return j.time.j(100 * 1000);}; break;
-        case 11: func = [](Judge& j) {return j.ret.j(true);}; break;
-        case 12: func = [](Judge& j) {return j.mileage.j(2000);}; break;
-        case 13: func = [](Judge& j) {return j.mileage.j(2500);}; break;
-        case 14: func = [](Judge& j) {return j.ret.j(true);}; break;
-        case 15: func = [](Judge& j) {return j.mileage.j(50);}; break;
-        case 16: func = [](Judge& j) {return j.ret.j(true);}; break;
-        case 17: func = [](Judge& j) {return j.mileage.j(2350);}; break;
-        case 18: func = [](Judge& j) {return j.ret.j(true);}; break;
+        case 1: func = [](Judge& j) {return j.ret.j(true);}; break;
+        case 2: func = [](Judge& j) {return j.ret.j(true) and j.time.j(1500 * 1000);}; break;
+        case 3: func = [](Judge& j) {return j.time.j(200 * 1000);}; break;
+        case 4: func = [](Judge& j) {return j.ret.j(true) and j.time.j(1500 * 1000);}; break;
+        case 5: func = [](Judge& j) {return j.time.j(100 * 1000);}; break;
+        case 6: func = [](Judge& j) {return j.button.j(4, true) or j.touch.j(true);}; break;
+        case 7: func = [](Judge& j) {return j.mileage.j(2700);}; break;
+        case 8: func = [](Judge& j) {return j.mileage.j(600);}; break;
+        case 9: func = [](Judge& j) {return j.mileage.j(1600);}; break;
+        case 10: func = [](Judge& j) {return j.mileage.j(550);}; break;
+        case 11: func = [](Judge& j) {return j.time.j(100 * 1000);}; break;
+        case 12: func = [](Judge& j) {return j.ret.j(true);}; break;
+        case 13: func = [](Judge& j) {return j.mileage.j(2000);}; break;
+        case 14: func = [](Judge& j) {return j.mileage.j(2500);}; break;
+        case 15: func = [](Judge& j) {return j.ret.j(true);}; break;
+        case 16: func = [](Judge& j) {return j.mileage.j(50);}; break;
+        case 17: func = [](Judge& j) {return j.ret.j(true);}; break;
+        case 18: func = [](Judge& j) {return j.mileage.j(2350);}; break;
         case 19: func = [](Judge& j) {return j.ret.j(true);}; break;
-        case 20: func = [](Judge& j) {return j.mileage.j(2000);}; break;
-        case 21: func = [](Judge& j) {return j.mileage.j(550);}; break;
-        case 22: func = [](Judge& j) {return j.mileage.j(1600);}; break;
-        case 23: func = [](Judge& j) {return j.time.j(100 * 1000);}; break;
-        case 24: func = [](Judge& j) {return j.ret.j(true);}; break;
+        case 20: func = [](Judge& j) {return j.ret.j(true);}; break;
+        case 21: func = [](Judge& j) {return j.mileage.j(2000);}; break;
+        case 22: func = [](Judge& j) {return j.mileage.j(550);}; break;
+        case 23: func = [](Judge& j) {return j.mileage.j(1600);}; break;
+        case 24: func = [](Judge& j) {return j.time.j(100 * 1000);}; break;
         case 25: func = [](Judge& j) {return j.ret.j(true);}; break;
-        case 26: func = [](Judge& j) {return j.time.j(100 * 1000);}; break;
+        case 26: func = [](Judge& j) {return j.ret.j(true);}; break;
+        case 27: func = [](Judge& j) {return j.time.j(100 * 1000);}; break;
     //#</func_list>
         default: return true;
     }
