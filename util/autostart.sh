4652dbd9ad9f3a11624bad63801540f67fcc5f1f
kiligiris <cays1200431@gn.iwasaki.ac.jp>
2023-10-03 17:01:41 +0900

    Merge autostart.sh

--- util/autostart.sh
+++ util/autostart.sh
@@ -1,5 +1,20 @@
+#!/bin/sh
 set -e  # エラーチェックを有効化
 
+# バックグラウンドで実行しているプロセスをプロセスグループとして指定
+pgid=$(ps -o pgid= $$ | awk '{print $1}')
+
+# Ctrl+Cが押下時、またはエラー時にバックグラウンドプロセスを強制終了
+trap 'kill -- -$pgid' INT
+trap catch EXIT
+
+catch(){
+    if [ "$?" != 0 ]; then
+        kill -- -$pgid
+    fi
+}
+
+# shファイルの存在するディレクトリにcd
 cd $(dirname "$0")
 
 cd ../
@@ -8,28 +23,28 @@ CFILE=$(basename $(pwd))
 # コマンドライン引数の処理
 fetchFLG=1
 simFLG=0
-#if [ "${1:0:1}" == "-" ]; then
-#    # 文字列の長さを取得
-#    length=${#1}
-#    # 文字列の一文字ずつ処理
-#    for ((i = 1; i < length; i++)); do
-#        # i番目の文字を取得
-#        char="${1:i:1}"
-#        
-#        if [ "$char" == "r" ]; then
-#            fetchFLG=0
-#        fi
-#
-#        if [ "$char" == "s" ]; then
-#            simFLG=1
-#        fi
-#    done
-#fi
+if [ "${1#-}" != "$1" ]; then
+    # 文字列の長さを取得
+    length=$(expr length "$1")
+    # 文字列の一文字ずつ処理
+    for i in $(seq 1 $length); do
+        # i番目の文字を取得
+        char=$(expr substr "$1" $i 1)
+        
+        if [ "$char" = "r" ]; then
+            fetchFLG=0
+        fi
 
-#if [ $fetchFLG == 1 ]; then
-git fetch origin main
-git reset --hard origin/main
-#fi
+        if [ "$char" = "s" ]; then
+            simFLG=1
+        fi
+    done
+fi
+
+if [ $fetchFLG = 1 ]; then
+    git fetch origin main
+    git reset --hard origin/main
+fi
 
 make -C background_tasks
 
@@ -37,18 +52,16 @@ cd ../
 
 mkdir -m 755 img || true
 # background_tasksをバックグラウンドで実行
-./$CFILE/background_tasks/background_tasks &
+./$CFILE/background_tasks/background_tasks & 
 
 chmod -R 755 ./$CFILE
 
-if [ $simFLG == 1 ]; then
+if [ $simFLG = 1 ]; then
     cd ../
     make app=$CFILE sim up
 else
     rm asp || true
     make img=$CFILE
-    # etrobo2023をバックグラウンドで実行
+    # メインタスクを実行
     make start
 fi
-
-kill %1
