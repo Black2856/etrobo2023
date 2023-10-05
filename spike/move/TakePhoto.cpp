5f39b99eb764808fd27fa1c6ba04329b21bbd531
kiligiris <cays1200431@gn.iwasaki.ac.jp>
2023-10-05 15:37:23 +0900

    fix

--- spike/move/TakePhoto.cpp
+++ spike/move/TakePhoto.cpp
@@ -23,9 +23,9 @@ void TakePhoto::first(int num) {
 
 bool TakePhoto::execute(){
     if (device.camera_getQueueSize() != 0) {
-        print("TakePhoto:false\n");
+        printf("TakePhoto:false\n");
         return false;
     }
-    print("TakePhoto:true\n");
+    printf("TakePhoto:true\n");
     return true;
 }
