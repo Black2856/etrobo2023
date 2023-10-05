9dea8a551f79515890516545e7e4b77f67f7cb33
kiligiris <cays1200431@gn.iwasaki.ac.jp>
2023-10-05 09:18:49 +0900

    fix

--- util/autostart.sh
+++ util/autostart.sh
@@ -58,7 +58,7 @@ cd ../
 # img保存用ディレクトリの作成
 mkdir -m 755 img || true
 # 撮影待機リストの初期化
-rm "Queue.txt" || true
+rm -f "Queue.txt" || true
 # background_tasksをバックグラウンドで実行
 ./$CFILE/background_tasks/background_tasks & 
 
